70d8c31abe60881a7f2475d0e750b61ff87d2e14
atelefoxs <120293973+atelefoxs@users.noreply.github.com>
2024-01-26 22:29:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var card = document.getElementById('movingCard');
+    var velocityX = 1;
+    var velocityY = 1;
+    var posX = 0;
+    var posY = 0;
+
+    function moveCard() {
+        posX += velocityX;
+        posY += velocityY;
+
+        if (posX + card.offsetWidth > window.innerWidth || posX < 0) {
+            velocityX *= -1;
+        }
+
+        if (posY + card.offsetHeight > window.innerHeight || posY < 0) {
+            velocityY *= -1;
+        }
+
+        card.style.left = posX + 'px';
+        card.style.top = posY + 'px';
+
+        requestAnimationFrame(moveCard);
+    }
+
+    moveCard();
+});
