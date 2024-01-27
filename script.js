da527aabe6ea95e6694b452aa05a842c0f6f13f0
atelefoxs <120293973+atelefoxs@users.noreply.github.com>
2024-01-27 19:35:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var card = document.getElementById('movingCard');
-    var velocityX = 15;
-    var velocityY = 15;
+    var velocityX = 10;
+    var velocityY = 10;
     var posX = 0;
     var posY = 0;
 
